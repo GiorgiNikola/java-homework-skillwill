2d0398681ae4106d127049e0a15e15f07126e0af
Giorgi Nikoladze <giorginikoladze963@gmail.com>
2025-04-06 20:14:28 +0400

    completed the files and exceptions exercise

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,26 @@
 package src;
 
+import java.io.*;
+
 public class Main {
+    public static String fileName = "src/sometext.txt";
+
     public static void main(String[] args) {
-        double num1 = 10;
-        double num2 = 14;
-        double num3 = 5;
-        double num4 = 305;
+        // try with resources ავტომატურად ხურავს რესურსებს
+        try(FileWriter fileWriter = new FileWriter(fileName)){
+            fileWriter.write("Here is some text\n");
+            fileWriter.write("Here is other text\n");
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName());
+        }
 
-        System.out.println(averageOfFourNumbers(num1, num2, num3, num4));
-
-        double num5 = 52;
-        double num6 = 3;
-        double num7 = 3000;
-        double num8 = 486;
-
-        System.out.println(averageOfFourNumbers(num5, num6, num7, num8));
-
-        String a = "something";
-        String b = "anything";
-        String c = "nothing";
-
-        System.out.println(lengthOfThreeStrings(a, b, c));
-
-        String d = "monday";
-        String e = "tuesday";
-        String f = "wednesday";
-
-        System.out.println(lengthOfThreeStrings(d, e, f));
-    }
-
-    /*
-    *  რადგან გვინდა საშუალოს გამოთვლა საჭიროა ისეთი დეითა ტაიპის
-    *  გამოყენება რომელსაც მთელი ნაწილიც აქვს და წილადიც ამიტომ პარამეტრად გადავცემთ დაბლს
-    * */
-    public static double averageOfFourNumbers(double a, double b, double c, double d) {
-        return (a + b + c + d) / 4;
-    }
-
-    // თითოეული სტრინგის სიგრძის გასაგებად ვიყენებთ ჩაშენებულ მეთოდს
-    public static int lengthOfThreeStrings(String a, String b, String c) {
-        return a.length() + b.length() + c.length();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName());
+        }
     }
 }
